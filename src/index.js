8b92b26f988f229b68365b0d06a15f54c540c346
贾贵山 <jiaguishan@mistong.com>
2018-08-07 17:47:02 +0800

    向外暴露url类方法

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import dateFormat from './dateFormat';
+import setUrlParam from './setUrlParam';
+import getUrlParam from './getUrlParam';
 
 export default {
   dateFormat,
+  setUrlParam,
+  getUrlParam,
 };
