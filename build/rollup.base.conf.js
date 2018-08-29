a89faadfb5b614e4a1c5c6fa3abd41fedb526901
yuhongming@mistong.com <yuhongming@mistong.com>
2018-08-29 11:25:25 +0800

    fix babel bug

--- build/rollup.base.conf.js
+++ build/rollup.base.conf.js
@@ -1,7 +1,7 @@
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
-import buble from 'rollup-plugin-buble';
-// import babel from 'rollup-plugin-babel';
+// import buble from 'rollup-plugin-buble';
+import babel from 'rollup-plugin-babel';
 import es3 from 'rollup-plugin-es3';
 import { eslint } from 'rollup-plugin-eslint';
 import pkg from '../package.json';
@@ -24,12 +24,13 @@ export default {
       include: 'src/**',
       exclude: ['node_modules/**', 'dist/**'],
     }),
-    buble({
-      objectAssign: 'Object.assign',
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
       include: ['src/**', 'test/**'],
     }),
     es3({
       remove: ['defineProperty', 'freeze']
-    })
+    }),
   ],
 };
