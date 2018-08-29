a6ae4b7911d73ffa9603e9130a04faf8e271752e
caiyue <caiyue@mistong.com>
2018-08-29 18:01:05 +0800

    cookie注释更新

--- src/getCookie.js
+++ src/getCookie.js
@@ -4,6 +4,8 @@
  * @since 1.1.0
  * @category storage;
  * @author caiyue823;
+ * @param name, {string} cookie name
+ * @param isObject, {boolean} is the return an object,using JSON.parse
  * @return value;
  * @create_date 2018/08/27;
  * @modify_date 2018/08/27;
