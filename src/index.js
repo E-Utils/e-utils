32f52a508a2c2813845f36ba119b49704ee284b9
yuhongming@mistong.com <yuhongming@mistong.com>
2018-08-29 17:51:26 +0800

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dateFormat from './dateFormat';
-import setUrlParam from './setUrlParam';
+// import setUrlParam from './setUrlParam';
 import getUrlParam from './getUrlParam';
 import replace from './replace';
 import replaceAll from './replaceAll';
@@ -7,15 +7,15 @@ import trim from './trim';
 import toUpper from './toUpper';
 import toLower from './toLower';
 import generateUUID from './generateUUID';
-import union from './union';
-import intersection from './intersection';
-import minus from './minus';
-import unique from './unique';
+// import union from './union';
+// import intersection from './intersection';
+// import minus from './minus';
+// import unique from './unique';
 import indexOf from './indexOf';
 
 export default {
   dateFormat,
-  setUrlParam,
+  // setUrlParam,
   getUrlParam,
   replaceAll,
   replace,
@@ -23,9 +23,9 @@ export default {
   toUpper,
   toLower,
   generateUUID,
-  union,
-  intersection,
-  minus,
-  unique,
+  // union,
+  // intersection,
+  // minus,
+  // unique,
   indexOf,
 };
