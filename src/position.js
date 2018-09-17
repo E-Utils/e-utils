92f9b9e6b23fcd5d07825c459a6fc03ee71de172
樊留 <fanliu@mistong.com>
2018-09-17 20:02:11 +0800

    add: dom操作方法加入 position, scrollTop

---
+++ src/position.js
@@ -0,0 +1,28 @@
+/**
+ * Gets the location of the element in the entire page.
+ *
+ * @since 1.2.0;
+ * @category dom;
+ * @author fl2294;
+ * @param elem {dom object} origin dom;
+ * @param type {string} defalut '';
+ * @return {number|object};
+ * @create_date 2018/09/17;
+ * @modify_date 2018/09/17;
+ * @example
+ *
+ * position(document.getElemenstsByTag('body')[0], 'top')
+ * position(document.getElemenstsByTag('body')[0], 'left')
+ * // => 0
+ *
+ * position(document.getElemenstsByTag('body')[0])
+ * // => {top: 0, left: 0}
+ */
+function position(elem, type='') {
+  return type.toLowerCase() === 'left' ?
+    elem.offsetLeft : (type.toLowerCase() === 'top' ?
+      elem.offsetTop : {
+        top: elem.offsetTop,
+        left: elem.offsetLeft
+      });
+}
--- src/scrollTop.js
+++ src/scrollTop.js
@@ -1,29 +1,46 @@
 /**
- * scrollTop, 返回指定元素dom滚动条的距离
+ * Get element scrolling distance
  *
- * @since 1.0.0;
+ * @since 1.2.0;
  * @category dom;
  * @author fl2294;
- * @param array {array} origin array;
- * @param value {number/string} find value;
- * @param fromIndex {number} start number;
- * @return {number};
- * @create_date 2018/09/09;
- * @modify_date 2018/09/09;
+ * @param elem {dom object} origin dom;
+ * @param target {number} find value;
+ * @return {number|void};
+ * @create_date 2018/09/17;
+ * @modify_date 2018/09/17;
  * @example
  *
- * indexOf([1, 2, 1, 2], 2)
- * // => 1
+ * scrollTop(document)
+ * // => 0
  *
- * // Search from the `fromIndex`
- * indexOf([1, 2, 1, 2], 2, 2)
- * // => 3
+ * // input in target
+ * scrollTop(document, 200)
+ * // => Scroll bar moves to 200.
  */
 
-function scrollTop() {
-  let y = (window.pageYOffset !== undefined)
-    ? window.pageYOffset
-    : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-
-
+function scrollTop(elem, target) {
+  function isWindow( elem ) {
+    return elem != null && elem === elem.window;
+  }
+  let prop = 'pageYOffset';
+  let win, top = 'pageYOffset' === prop;
+  if ( isWindow( elem ) ) { // window
+    win = elem;
+  } else if ( elem.nodeType === 9 ) { // document
+    win = elem.defaultView;
+  }
+  if ( target === undefined ) {
+    // window|document => window.pageYOffset
+    // dom => elem.scrollTop
+    return win ? win[prop] : elem.scrollTop;
+  }
+  if ( win ) {
+    win.scrollTo(
+      !top ? target : win.pageXOffset,
+      top ? target : win.pageYOffset
+    );
+  } else {
+    elem.scrollTop = target;
+  }
 }
